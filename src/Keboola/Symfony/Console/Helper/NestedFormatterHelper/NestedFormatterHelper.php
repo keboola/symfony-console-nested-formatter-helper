d690ff62d7524d87fe3e3bf53d1c6651748bf1d4
Odin <odinuv@gmail.com>
2016-05-10 08:31:31 +0200

    feat: update symfony to v3

--- src/Keboola/Symfony/Console/Helper/NestedFormatterHelper/NestedFormatterHelper.php
+++ src/Keboola/Symfony/Console/Helper/NestedFormatterHelper/NestedFormatterHelper.php
@@ -13,37 +13,37 @@ use Symfony\Component\Console\Helper\FormatterHelper;
 class NestedFormatterHelper extends FormatterHelper
 {
 
-	/**
-	 * @param array $hashmap
-	 * @return string
-	 */
-	public function format(array $hashmap, $indentLevel = 0)
-	{
-		$prefix = str_repeat(' ', 2 * $indentLevel);
-		$output = '';
-		$isAssocArray = $this->isAssocArray($hashmap);
-		foreach ($hashmap as $key => $value) {
-			if (is_array($value)) {
-				$output .= sprintf("$prefix%s\n", $isAssocArray ? "$key:" : '-');
-				$output .= $this->format($value, $indentLevel + 1);
-			} else {
-				$output .= sprintf("$prefix%s\n", $isAssocArray ? "$key: $value" : "- $value");
-			}
-		}
-		return $output;
-	}
+    /**
+     * @param array $hashmap
+     * @return string
+     */
+    public function format(array $hashmap, $indentLevel = 0)
+    {
+        $prefix = str_repeat(' ', 2 * $indentLevel);
+        $output = '';
+        $isAssocArray = $this->isAssocArray($hashmap);
+        foreach ($hashmap as $key => $value) {
+            if (is_array($value)) {
+                $output .= sprintf("$prefix%s\n", $isAssocArray ? "$key:" : '-');
+                $output .= $this->format($value, $indentLevel + 1);
+            } else {
+                $output .= sprintf("$prefix%s\n", $isAssocArray ? "$key: $value" : "- $value");
+            }
+        }
+        return $output;
+    }
 
-	private function isAssocArray($array)
-	{
-		return array_keys($array) !== range(0, count($array) - 1);
-	}
+    private function isAssocArray($array)
+    {
+        return array_keys($array) !== range(0, count($array) - 1);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getName()
-	{
-		return 'nestedFormatter';
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'nestedFormatter';
+    }
 
 }
--- tests/NestedFormatterTest.php
+++ tests/NestedFormatterTest.php
@@ -9,99 +9,95 @@
 
 namespace Keboola\Symfony\Console\Helper\NestedFormatterHelper\Tests;
 
-
 use Keboola\Symfony\Console\Helper\NestedFormatterHelper\NestedFormatterHelper;
 
 class NestedFormatterTest extends \PHPUnit_Framework_TestCase
 {
+    public function testOneLevelHashmap()
+    {
+        $formatter = new NestedFormatterHelper();
 
+        $input = array(
+            'first' => 'something',
+            'second' => 'another',
+            'third' => 'haha',
+        );
+        $expected = "first: something\n";
+        $expected .= "second: another\n";
+        $expected .= "third: haha\n";
 
-	public function testOneLevelHashmap()
-	{
-		$formatter = new NestedFormatterHelper();
+        $this->assertEquals($expected, $formatter->format($input));
+    }
 
-		$input = array(
-			'first' => 'something',
-			'second' => 'another',
-			'third' => 'haha',
-		);
-		$expected = "first: something\n";
-		$expected .= "second: another\n";
-		$expected .= "third: haha\n";
+    public function testDefaultIndent()
+    {
+        $formatter = new NestedFormatterHelper();
 
-		$this->assertEquals($expected, $formatter->format($input));
-	}
+        $input = array(
+            'first' => 'something',
+            'second' => 'another',
+            'third' => 'haha',
+        );
+        $expected = "  first: something\n";
+        $expected .= "  second: another\n";
+        $expected .= "  third: haha\n";
 
-	public function testDefaultIndent()
-	{
-		$formatter = new NestedFormatterHelper();
+        $this->assertEquals($expected, $formatter->format($input, 1));
+    }
 
-		$input = array(
-			'first' => 'something',
-			'second' => 'another',
-			'third' => 'haha',
-		);
-		$expected = "  first: something\n";
-		$expected .= "  second: another\n";
-		$expected .= "  third: haha\n";
+    public function testOneLevelNumericArray()
+    {
+        $formatter = new NestedFormatterHelper();
+        $input = array(
+            'one',
+            'two',
+            'three',
+        );
+        $expected = "- one\n";
+        $expected .= "- two\n";
+        $expected .= "- three\n";
+        $this->assertEquals($expected, $formatter->format($input));
+    }
 
-		$this->assertEquals($expected, $formatter->format($input, 1));
-	}
+    public function testNestedHashmap()
+    {
+        $formatter = new NestedFormatterHelper();
+        $input = array(
+            'first' => 'something',
+            'second' => array(
+                'item 1' => 'val 1',
+                'item 2' => 'val 2',
+                'item 3' => array(
+                    'sub item 1' => 'value 1',
+                    'sub item 2' => 'value 2',
+                ),
+                'item 4' => array(
+                    'one',
+                    array(
+                        'nested',
+                        'more',
+                    ),
+                    'three',
+                )
+            ),
+            'third' => 'haha',
+        );
 
-	public function testOneLevelNumericArray()
-	{
-		$formatter = new NestedFormatterHelper();
-		$input = array(
-			'one',
-			'two',
-			'three',
-		);
-		$expected = "- one\n";
-		$expected .= "- two\n";
-		$expected .= "- three\n";
-		$this->assertEquals($expected, $formatter->format($input));
-	}
+        $expected = "first: something\n";
+        $expected .= "second:\n";
+        $expected .= "  item 1: val 1\n";
+        $expected .= "  item 2: val 2\n";
+        $expected .= "  item 3:\n";
+        $expected .= "    sub item 1: value 1\n";
+        $expected .= "    sub item 2: value 2\n";
+        $expected .= "  item 4:\n";
+        $expected .= "    - one\n";
+        $expected .= "    -\n";
+        $expected .= "      - nested\n";
+        $expected .= "      - more\n";
+        $expected .= "    - three\n";
+        $expected .= "third: haha\n";
 
-	public function testNestedHashmap()
-	{
-		$formatter = new NestedFormatterHelper();
-		$input = array(
-			'first' => 'something',
-			'second' => array(
-				'item 1' => 'val 1',
-				'item 2' => 'val 2',
-				'item 3' => array(
-					'sub item 1' => 'value 1',
-					'sub item 2' => 'value 2',
-				),
-				'item 4' => array(
-					'one',
-					array(
-						'nested',
-						'more',
-					),
-					'three',
-				)
-			),
-			'third' => 'haha',
-		);
-
-		$expected = "first: something\n";
-		$expected .= "second:\n";
-		$expected .= "  item 1: val 1\n";
-		$expected .= "  item 2: val 2\n";
-		$expected .= "  item 3:\n";
-		$expected .= "    sub item 1: value 1\n";
-		$expected .= "    sub item 2: value 2\n";
-		$expected .= "  item 4:\n";
-		$expected .= "    - one\n";
-		$expected .= "    -\n";
-		$expected .= "      - nested\n";
-		$expected .= "      - more\n";
-		$expected .= "    - three\n";
-		$expected .= "third: haha\n";
-
-		$this->assertEquals($expected, $formatter->format($input));
-	}
-
+        $this->assertEquals($expected, $formatter->format($input));
+    }
 }
